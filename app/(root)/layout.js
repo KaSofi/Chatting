1781f9adb3076bc85b8c0b7917e53b56483925da
KaSofi <for2879@yandex.ru>
2024-01-27 22:09:04 +0300

    first commit

---
+++ app/(root)/layout.js
@@ -0,0 +1,26 @@
+import { Inter } from "next/font/google";
+import "../globals.css";
+import Provider from "@components/Provider";
+import Navbar from "@components/Navbar";
+import BottomBar from "@components/BottomBar";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "Chatting Project",
+  description: "Chat App Next.js MongoDB",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={`${inter.className} bg-blue-2`}>
+        <Provider>
+        <Navbar/>
+        {children}
+        <BottomBar/>
+        </Provider>
+      </body>
+    </html>
+  );
+}
